6bde0f0210b54ff0baf941d8d53f82c9b34bdcf2
Bradley Tse <bradleytse@gmail.com>
2013-04-26 04:03:11 -0400

    Added all files. Currently airplane bg with orange header.

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/* 
+ * Author: Bradley Tse
+ * Email: bradleytse@gmail.com
+ * Date: April 2013
+ * 
+ */
+
+$(document).ready(function() {
+
+    /*
+     * Sticky nav bar 
+     */
+    var $top1 = $('#navbar').offset().top;
+
+    $(window).scroll(function() {   
+
+            if ($(window).scrollTop()>$top1) {
+                $('#navbar').addClass('sticky');
+            } else {
+                $('#navbar').removeClass('sticky');
+            }
+    });
+
+
+    /*
+     * Smooth scrolling animation
+     */
+    function goToByScroll(id) {
+        id = id.replace("link", "");
+
+        /*
+         * Returns if already at correct position 
+         * FIXME: find a better solution 
+         */
+        var $curPos = $(this).scrollTop();
+        var $goalPos = $("#" + id).offset().top;
+        if ($curPos == $goalPos) {
+            return;
+        }
+
+        $("html, body").stop(true).animate({scrollTop: $("#" + id).offset().top}, 700);
+    }
+
+    $("#navbar > ul > li > a").click(function(event) {
+        event.preventDefault(); 
+       
+        goToByScroll($(this).attr("id")); 
+    });
+
+    /*
+     * Fade in and out social link images
+     */
+    $("#social-links a").hover(
+        function() {
+            $(this).find("img").stop(true).fadeTo(300, 1);
+        },
+        function() {
+            $(this).find("img").stop(true).fadeTo(300, 0.4);
+        }
+    );
+    
+    /*
+     * Fade in and out menu items
+     */
+    $("#navbar > ul > li > a").hover(
+        
+    );
+
+});
